10daaa7a4d5bcfd8ef29a1d506898f02b43251eb
Your Name <you@example.com>
2019-10-20 16:55:14 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Friend-Finder
+Friend Finder
+About
+Friend Finder is a full-stack application that matches users to a character from NBC's "The Good Place" based on survey results. Node.js & an Express server on the back-end handle routing and connect to the "friends" API, which stores the character data. HTML & CSS(with a Bootstrap framework) display the landing page, survey, and user's match (via a modal pop-up) on the front-end. The application is deployed to Heroku.
+
+Technologies Used
+Git
+GitHub
+HTML
+CSS
+Bootstrap
+JavaScript
+jQuery
+Node.js
+Express.js
+Heroku
---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+const friends = [
+    {
+       "name": "Eleanor Shellstrop", 
+       "photo": "https://media.giphy.com/media/3ohs7QsBjK2EkvlLuU/giphy.gif",
+       "scores":[
+            5, 
+            2, 
+            4, 
+            3, 
+            1, 
+            5, 
+            1, 
+            4, 
+            1, 
+            1
+       ]
+    },
+    {
+      "name": "Jason Mendoza", 
+      "photo": "https://media.giphy.com/media/3ohs80ubv3iyojPquY/giphy.gif",
+      "scores":[
+           4, 
+           5, 
+           3, 
+           5, 
+           1, 
+           1, 
+           1, 
+           4, 
+           1, 
+           1
+      ]
+     }, 
+     {
+      "name": "Michael", 
+      "photo": "https://media.giphy.com/media/xT0xes9FP7JVi8LxoA/giphy.gif",
+      "scores":[
+           1, 
+           1, 
+           5, 
+           3, 
+           4, 
+           1, 
+           1, 
+           4, 
+           2, 
+           5
+      ]
+     }, 
+     {
+      "name": "Tahani Al Jamil", 
+      "photo": "https://media.giphy.com/media/3o6fJ2njgvaC49hQli/giphy.gif",
+      "scores":[
+           1, 
+           1, 
+           4, 
+           1, 
+           5, 
+           1, 
+           1, 
+           5, 
+           3, 
+           3
+      ]
+     }, 
+];
+
+module.exports = friends;
